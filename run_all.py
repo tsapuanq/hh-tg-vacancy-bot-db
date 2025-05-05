d326a23f9e87bd01c178de858b176b8088665d06
Sapuan <sapuantalaspai@gmail.com>
2025-05-06 01:47:26 +0500

    поменял правильно логирование

--- run_all.py
+++ run_all.py
@@ -1,12 +1,13 @@
 import os
 import asyncio
 import pandas as pd
+import logging
 from datetime import datetime
 from src.run import run_scraper
 from src.cleaning import run_cleaning_pipeline
 from src.publisher import run_publisher
 from src.config import CSV_MAIN, PROCESSED_DIR
-
+from src.utils import setup_logger
 def determine_mode() -> str:
     if not os.path.exists(CSV_MAIN):
         return "full"
@@ -24,19 +25,21 @@ def save_to_csv(df: pd.DataFrame):
     print(f"[INFO] Saved cleaned data to: {full_path}")
 
 if __name__ == "__main__":
+    setup_logger()
+    
     mode = determine_mode()
-    print(f"[MODE] Выбран режим: {mode.upper()}")
+    logging.info(f"[MODE] Выбран режим: {mode.upper()}")
 
-    print("[STEP 1] Scraping raw data...")
+    logging.info("[STEP 1] Scraping raw data...")
     raw = asyncio.run(run_scraper(mode=mode))
 
-    print("\n[STEP 2] Cleaning data...")
+    logging.info("[STEP 2] Cleaning data...")
     cleaned = run_cleaning_pipeline(raw)
 
-    print("\n[STEP 3] Saving to CSV...")
+    logging.info("[STEP 3] Saving to CSV...")
     save_to_csv(cleaned)
 
-    print("\n[STEP 4] Publishing to Telegram...")
+    logging.info("[STEP 4] Publishing to Telegram...")
     asyncio.run(run_publisher())
 
-    print("\n✅ Pipeline completed successfully!")
+    logging.info("✅ Pipeline completed successfully!")
