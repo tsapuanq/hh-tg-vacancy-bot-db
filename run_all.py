a665c26d9a2431684e40fc7ec47df135600116cf
Sapuan <sapuantalaspai@gmail.com>
2025-05-06 12:49:03 +0500

    оставил только __main__

--- run_all.py
+++ run_all.py
@@ -1,32 +1,15 @@
-import os
 import asyncio
-import pandas as pd
 import logging
-from datetime import datetime
-from src.run import run_scraper
+from src.run_scraper import run_scraper
 from src.cleaning import run_cleaning_pipeline
 from src.publisher import run_publisher
-from src.config import CSV_MAIN, PROCESSED_DIR
 from src.utils import setup_logger
-def determine_mode() -> str:
-    if not os.path.exists(CSV_MAIN):
-        return "full"
-    try:
-        df = pd.read_csv(CSV_MAIN)
-        return "full" if df.empty else "daily"
-    except Exception:
-        return "full"
-
-def save_to_csv(df: pd.DataFrame):
-    os.makedirs(PROCESSED_DIR, exist_ok=True)
-    filename = f"vacancies_clean_{datetime.now().strftime('%Y-%m-%d')}.csv"
-    full_path = os.path.join(PROCESSED_DIR, filename)
-    df.to_csv(full_path, index=False)
-    print(f"[INFO] Saved cleaned data to: {full_path}")
+from src.utils import determine_mode
+from src.utils import save_daily_clean_csv
 
 if __name__ == "__main__":
     setup_logger()
-    
+
     mode = determine_mode()
     logging.info(f"[MODE] Выбран режим: {mode.upper()}")
 
@@ -37,7 +20,7 @@ if __name__ == "__main__":
     cleaned = run_cleaning_pipeline(raw)
 
     logging.info("[STEP 3] Saving to CSV...")
-    save_to_csv(cleaned)
+    save_daily_clean_csv(cleaned)
 
     logging.info("[STEP 4] Publishing to Telegram...")
     asyncio.run(run_publisher())
